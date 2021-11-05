35b3f38e5af868c0f3e7a15d236f8ce79f8706d7
Slava <example@gmail.com>
2021-11-05 13:13:08 +0300

    Block precent

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,9 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+const counters = document.querySelectorAll('.skills__percent-text'),
+      lines = document.querySelectorAll('.skills__percent-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
